d30af450a854e1257c9a6e8776f9bee7a574aaba
Chris <ceequick@gmail.com>
2018-03-26 17:58:03 -0500

    Functional code
    Updated from tags2.js

--- tags.js
+++ tags.js
@@ -1,40 +1,41 @@
-var divGame;
-var gameTags = ["assassin's creed", "bioshock infinite", "dai", "dota 2", "dxmd", "fallout", "infamous", "lol", "mass effect", "me", "mec", "nmh", "off", "portal", "remember me", "saints row", "skyrim", "sonic", "ssb", "superbrothers", "tf2", "the division", "tlou", "twau", "undertale", "watch_dogs","assassin's creed", "bioshock infinite", "dai", "dota 2", "dxmd", "fallout", "infamous", "lol", "mass effect", "me", "mec", "nmh", "off", "portal", "remember me", "saints row", "skyrim", "sonic", "ssb", "superbrothers", "tf2", "the division", "tlou", "twau", "undertale", "watch_dogs"];
+var divTags;
+var divBorders;
+var tagList1 = ["assassin's creed", "bioshock infinite", "dai", "dota 2", "dxmd", "fallout", "infamous", "lol", "mass effect", "me", "mec", "nmh", "off", "portal", "remember me", "saints row", "skyrim", "sonic", "ssb", "superbrothers", "tf2", "the division", "tlou", "twau", "undertale", "watch_dogs","assassin's creed", "bioshock infinite", "dai", "dota 2", "dxmd", "fallout", "infamous", "lol", "mass effect", "me", "mec", "nmh", "off", "portal", "remember me", "saints row", "skyrim", "sonic", "ssb", "superbrothers", "tf2", "the division", "tlou", "twau", "undertale", "watch_dogs"];
 var iconLoc = '<img src="http://cquickdesign.com/misc/tmblr/';
 
-var x = gameTags.length;
+var x = tagList1.length;
 
 //Show icons of all the tags. "document.write" needs to be changed to innerHTML. 5 icons per row.
 
 
 function splitTags(){
     
-    divGame = document.getElementById("g");
+    divTags = document.getElementById("tags1");
 
     for(i=0; i<x; i++){
         
         var addSpan = document.createElement("span");
-        divGame.appendChild(addSpan);
+        divTags.appendChild(addSpan);
         //spanItem must be declared outside of function
         var spanItem;
         
         //alert(i % 5);
-        gameTags.sort();
+        tagList1.sort();
         
         //If the remainder of i % 5 = 4, start a new line
         if(i % 5 === 4){
-            //divGame.innerHTML = iconLoc + gameTags[i] + '.jpg" /><br/>';
+            //divTags.innerHTML = iconLoc + gameTags[i] + '.jpg" /><br/>';
             spanItem = document.getElementsByTagName('span').item([i]);
-            spanItem.innerHTML = iconLoc + gameTags[i] + '.jpg" /><br/>';
+            spanItem.innerHTML = iconLoc + tagList1[i] + '.jpg" /><br/>';
         } else {
-            //divGame.innerHTML = iconLoc + gameTags[i] + '.jpg" />';
+            //divTags.innerHTML = iconLoc + gameTags[i] + '.jpg" />';
             spanItem = document.getElementsByTagName('span').item([i]);
-            spanItem.innerHTML = iconLoc + gameTags[i] + '.jpg" />';
+            spanItem.innerHTML = iconLoc + tagList1[i] + '.jpg" />';
         }
     }
 }
 
-/*function borders(){
+function borders(){
     
     for(i=0; i<x; i++){
         
@@ -46,12 +47,12 @@ function splitTags(){
         
     }
     
-}*/
+}
 
 
 function tagpage(){
     splitTags();
-    //borders();
+    borders();
 }
 
 window.onload = tagpage;
