636c7231df0e274c5ae26645a82b1da64403139f
Chris <ceequick@gmail.com>
2018-04-04 21:37:38 -0500

    Acknowledging the ul elements via spans

--- tags.js
+++ tags.js
@@ -1,54 +1,76 @@
 var getLists = document.getElementsByTagName('ul');
 var getListsNum = document.getElementsByTagName('ul').length;
-var x = getListsNum;
+var x;
 
-//var getListChilds = getLists.childNodes;
 
 var showListNumber = document.getElementById("info");
 var showListNumber2 = document.getElementById("info2");
 
 
 
+function getListItems(){  //Retrieve <li> items within a <ul> tag
+
+    alert("Running function 'getListItems'");
+
+    var childListItem = this.childNodes;
+
+    var x = getListsNum;
+
+//Debugging
+    for(i = 0; i < x; i++){
+
+        alert("Creating a span element");
+
+        var addSpan = document.createElement("span");
+
+        alert("Appending a span");
+        showListNumber.appendChild(addSpan);
+
+        spanItem = document.getElementsByTagName("span").item([i]);
+        spanItem.innerHTML = "This is a generated span.<br/>";
+
+        //showListNumber.innerHTML = childListItem;
+        showListNumber2.innerHTML = 'There are ' + x + ' &lt;ul&gt; items';
 
 
 
+    }
 
+    alert("Ending function 'getListItems'");
 
-
-function displayInfo(){
-    showListNumber.innerHTML = "There are " + getListsNum +" tag lists on this page.";
 }
 
+//var func1 = getListItems();
 
 
+function createArr(func1){
 
-
-
-function createArr(){
+    alert("Running function 'createArr'");
 
     var tagGenres = []; //Create parent array to contain other arrays
 
-    var fetchList;
-
     for(i = 0; i < x; i++){
 
-        //tagGenres[i] = [];
-        alert(i); //Should detect 5 UL tags
-        fetchList = getLists.item([i]);
-        //var getChildren = fetchList.childNodes;
-        showListNumber2.innerHTML = getChildren[i];
+        tagGenres[i] = getLists.item([i]);
 
-        //if(fetchList.hasChildNodes === true)
-    }
+    } alert("Ending for loop");
 
     return tagGenres;
 
+alert("Ending function 'createArr'");
 }
 
-//function accessArr(tagGenres){
 
+function displayInfo(y){
 
-//}
+    alert("Running function 'displayInfo'");
+
+    showListNumber2.innerHTML = "There are " + getListsNum +" tag lists on this page.";
+    if(tagGenres === true){
+        alert("True");
+    }
+}
+
 
 
 
@@ -57,9 +79,9 @@ function createArr(){
 
 
 function runAll(){
-    displayInfo();
-    createArr();
-    accessArr();
+    getListItems();
+    //createArr();
+    //displayInfo();
 }
 
 window.onload = runAll;
